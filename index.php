be602f29b898993a6f2bde063b61c398f7bd0e3c
vezhdelit <vezhdel.vasya2001@gmail.com>
2021-12-13 09:52:23 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles/style.css">
+    <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico" rel="stylesheet" type='text/css'>
+    <title>Meme Generator</title>
+</head>
+<body>
+<h1>Memes Generator</h1>
+<div class="container shadow p-4 bg-body rounded">
+    <canvas id="canvas"></canvas>
+    <div class="sidebar">
+        <input type="file" id="file" class="form-control">
+
+        <div class="group">
+            <textarea class="form-control" placeholder="Type something" id="text"></textarea>
+
+            <label for="fontFamily">Font family</label>
+            <p><select id="fontFamily" class="form-select">
+                    <option value="Roboto">Roboto</option>
+                    <option value="Times New Roman">Times New Roman</option>
+                    <option value="Impact">Impact</option>
+                    <option value="Pacifico">Pacifico</option>
+                    <option value="Montserrat">Montserrat</option>
+                    <option value="Arial">Arial</option>
+                </select>
+            </p>
+
+            <label for="color">Color</label>
+            <input type="color" id="color">
+
+            <label for="opacity">Opacity</label>
+            <input type="range" value="1"  step="0.05" id="opacity" min="0" max="1">
+
+            <div class="form-check form-switch">
+                <input class="form-check-input" type="checkbox" id="strokeCheckbox">
+                <label class="form-check-label" for="strokeCheckbox">Enable text stroke</label>
+            </div>
+
+
+            <button class="btn btn-primary" id="addText">Add new text</button>
+        </div>
+
+        <button class="btn btn-success" id="save">Save result</button>
+    </div>
+</div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fabric.js/460/fabric.js" integrity="sha512-d5yqsvICrC8y0ivNsNizSCHXjzireXYU6LmzAvcrL97GMwEn0VKx1vclEwvtsh/yPD2EcATnUG1oEtJuFcTE3Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="main.js"></script>
+</body>
+</html>
---
+++ main.js
@@ -0,0 +1,74 @@
+let file = document.getElementById('file')
+
+const canvas = new fabric.Canvas('canvas', {
+    width: 520,
+    height: 520 ,
+    backgroundColor: '#0f0f0f'
+})
+
+file.addEventListener('change', function(){
+    let img = file.files[0]
+    if(!img){
+        return
+    }
+    let reader = new FileReader()
+    canvas.setDim
+    reader.onload = function(e){
+        let data = reader.result
+        fabric.Image.fromURL(data, function(img){
+            img.scaleToHeight(500);
+            let scaleFactor = 500/img.height;
+
+            width = img.width * scaleFactor;
+            height = 500;
+
+            canvas.clear();
+            canvas.setDimensions({ width, height });
+            canvas.setBackgroundImage(img);
+
+        })
+    }
+    reader.readAsDataURL(img)
+})
+
+let addTextBtn = document.getElementById('addText')
+let text = document.getElementById('text')
+let fontFamily = document.getElementById('fontFamily')
+let color = document.getElementById('color')
+let opacity = document.getElementById('opacity')
+let strokeCheckbox = document.getElementById('strokeCheckbox')
+
+addTextBtn.addEventListener('click', function(){
+
+    let _text = new fabric.IText(text.value, {
+        editable: true,
+        left: 100,
+        top: 100,
+        fontSize: 64,
+        fontFamily: fontFamily.options[fontFamily.selectedIndex].value,
+        opacity: opacity.value,
+        fill: color.value,
+    })
+    if(strokeCheckbox.checked){
+        _text.stroke = '#000';
+        _text.strokeWidth = 5;
+        _text.paintFirst = 'stroke';
+    }
+    canvas.add(_text)
+})
+
+window.addEventListener('keydown', function(e){
+    if(e.key == "Delete"){
+        canvas.remove(canvas.getActiveObject())
+    }
+})
+
+let saveBtn = document.getElementById('save')
+saveBtn.addEventListener('click', function(){
+    let data = canvas.toDataURL()
+    let link = document.createElement('a')
+    link.href = data
+    link.download = 'image.png'
+    link.click()
+})
+
